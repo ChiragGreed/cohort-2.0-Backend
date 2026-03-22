2c53c19168145369798c9b726f13d82294d3614d
Chirag Greed <chiraggreed02@gmail.com>
2026-03-22 20:54:25 +0530

    Added email sending tool for LLM model

--- GenAi/chat.js
+++ GenAi/chat.js
@@ -1,29 +1,61 @@
-import { createInterface } from "readline";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { HumanMessage } from "langchain";
+import { createAgent, HumanMessage, tool } from "langchain";
+import readline from "readline/promises";
+import * as z from 'zod'
 import 'dotenv/config'
 
-const rl = createInterface({
+import sendEmail from "./services/emailService.js";
+
+
+const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
-});
+  output: process.stdout
+})
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
   apiKey: process.env.API_KEY,
 });
 
+const emailSendTool = tool(
 
-const messages = ['hey'];
+  sendEmail,
+  {
+    name: "emailSending",
+    description: "Tool for sending an email to someone",
+    schema: z.object({
+      to: z.string().describe("Email address of the recipient"),
+      subject: z.string().describe("Subject of the email we are sending"),
+      html: z.string().describe("Html format of the email , use some good formating")
+    })
+  }
+)
+
+const agent = createAgent({
+  model,
+  tools: [emailSendTool],
+  systemPrompt: `
+whenever you are told to send emails.
+
+Important:
+- Only send ONE email per user request
+- Never resend previous emails
+- Ignore previous tool calls
+`
+
+})
+
+const messages = [];
 
 while (true) {
-  const userInput = await rl.question("You:");
+
+  const userInput = await rl.question("You: ");
 
   messages.push(new HumanMessage(userInput));
-  
-  const response = await model.invoke(messages);
-  messages.push(response);
-  
-  console.log(response.text);
 
+  const response = await agent.invoke({ messages });
+
+  console.log(response.messages[response.messages.length - 1].content);
+
+  messages.push(response.messages[response.messages.length - 1]);
 }
---
+++ GenAi/services/emailService.js
@@ -0,0 +1,43 @@
+import nodemailer from 'nodemailer'
+
+
+const tranporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: process.env.GOOGLE_USER,
+        clientId: process.env.GOOGLE_CLIENT_URI,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN
+    }
+})
+
+tranporter.verify((error, success) => {
+    if (error) {
+        console.log('Error connecting to email server:', error)
+    }
+    else {
+    }
+})
+
+
+
+const sendEmail = async ({to, subject, html}) => {
+
+    try {
+        const info = await tranporter.sendMail({
+            from: `chiraggreed02@gmail.com ${process.env.GOOGLE_USER}`,
+            to,
+            subject,
+            html
+        })
+
+        return console.log("Email sent successfully to " + to)
+    }
+    catch (err) {
+        console.log(`Error sending email to ${to} `, err)
+    }
+
+}
+
+export default sendEmail
